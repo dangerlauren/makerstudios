e0875a5898b98a428b5a2f14a957be6496f441d5
Grant <grantlauren@wireless-10-147-197-23.public.utexas.edu>
2017-11-17 15:51:46 -0600

    facilities update

--- index.php
+++ index.php
@@ -37,7 +37,16 @@ $doc->addScript($this->baseurl.'/templates/'.$this->template.'/javascript/makers
 	<jdoc:include type="head" />
 </head>
 <body<?php echo $pageclass;?>>
-  <?php include_once("analyticstracking.php") ?>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92015758-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 	<div id="wrapper">
 		<div id="mast">
 			<jdoc:include type="modules" name="mast" style="none" />
